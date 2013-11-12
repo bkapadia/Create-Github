de7e3c1fd4f07087f3dfba0e1c6b5652f3c67d53
bkap <bkapadia@ryerson.ca>
2013-11-12 17:07:02 -0500

    attempt1

---
+++ gittrial1/src/gittrial1.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : gittrial1.cpp
+// Author      : Bhavin Kapadia
+// Version     :
+// Copyright   : bkapadia
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello world" << endl; // prints
+	return 0;
+}
